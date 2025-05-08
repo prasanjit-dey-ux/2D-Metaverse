19cf516c87081931b4fcd5ff91bb3a2c4d1dd2e7
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-05-08 22:58:22 +0530

    commit

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -4,11 +4,13 @@ import { userRouter } from "./userRouter";
 import { spaceRouter } from "./spaceRouter";
 import { elementRouter } from "./elementRouter";
 import { adminRouter } from "./adminRouter";
+import { avatarRouter } from "./avatarRouter";
 
 export const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/user", userRouter);
 router.use("/space", spaceRouter);
-router.use("/element,", elementRouter);
-router.use("/admin", adminRouter)
+router.use("/elements", elementRouter);
+router.use("/admin", adminRouter);
+router.use("/avatars", avatarRouter);
