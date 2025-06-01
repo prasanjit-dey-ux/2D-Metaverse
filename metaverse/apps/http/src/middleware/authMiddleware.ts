5d32e07b47ebb65742f96c078e98c0b19b932d41
prasanjit-dey-ux <prasanjitdey8523@gmail.com>
2025-06-01 18:00:59 +0000

    added middleware

--- metaverse/apps/http/src/middleware/authMiddleware.ts
+++ metaverse/apps/http/src/middleware/authMiddleware.ts
@@ -1,1 +1,7 @@
 import { Request, Response, NextFunction} from "express";
+import { generateJWT } from "@metaverse/utils";
+
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    
+}
