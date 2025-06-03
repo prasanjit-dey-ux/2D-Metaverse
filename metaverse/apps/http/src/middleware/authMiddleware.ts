85f742d0a64f9153a4423d4c007a5b2bcaf3647e
prasanjit-dey-ux <prasanjitdey8523@gmail.com>
2025-06-03 18:23:28 +0000

    update middleware

--- metaverse/apps/http/src/middleware/authMiddleware.ts
+++ metaverse/apps/http/src/middleware/authMiddleware.ts
@@ -1,7 +1,24 @@
 import { Request, Response, NextFunction} from "express";
-import jwt from "jsonwebtoken"
+
+declare global {
+    namespace Express {
+        
+    }
+}
 
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(" ")[1];
     
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: "No token provided"
+        });
+    }
+
+
+
+
+
 }
