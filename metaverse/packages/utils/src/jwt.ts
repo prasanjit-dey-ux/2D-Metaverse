dbd17b3122840d20c90d9b9a3f646b44e8028d33
prasanjit-dey-ux <prasanjitdey8523@gmail.com>
2025-06-09 18:21:02 +0000

    Added verifyJwt

--- metaverse/packages/utils/src/jwt.ts
+++ metaverse/packages/utils/src/jwt.ts
@@ -1,10 +1,27 @@
 import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
+if (!JWT_SECRET) throw new Error("Missing JWT_SECRET");
+
+
+export interface JwtPayload {
+    userId: string;
+    iat: number;
+    exp: number
+}
 
 export const generateJWT = (userId: string) => {
     return jwt.sign({ userId }, JWT_SECRET, {expiresIn:"7d"});
 };
+
+export const verifyJWT = (token: string): JwtPayload => {
+    try {
+        return jwt.verify(token, JWT_SECRET) as JwtPayload;
+    } catch (err) {
+        throw new Error ("Invalid or expired token");
+    }
+}
+
 
 
 
