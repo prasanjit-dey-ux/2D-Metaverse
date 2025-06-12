0b04466759e9f6d8634fb26953b689d34f988fe1
prasanjit-dey-ux <prasanjitdey8523@gmail.com>
2025-06-12 18:02:29 +0000

    updated db models

---
+++ metaverse/packages/db/prisma/migrations/20250611094251_init/migration.sql
@@ -0,0 +1,160 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('Admin', 'User');
+
+-- CreateEnum
+CREATE TYPE "AuthProvider" AS ENUM ('EMAIL', 'GOOGLE', 'GITHUB');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "tag" TEXT NOT NULL,
+    "displayName" TEXT,
+    "profileImageUrl" TEXT,
+    "bannerImageUrl" TEXT,
+    "avatarId" TEXT,
+    "bio" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'User',
+    "isProfileComplete" BOOLEAN NOT NULL DEFAULT false,
+    "provider" "AuthProvider" NOT NULL DEFAULT 'EMAIL',
+    "providerId" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Otp" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Otp_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Space" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "width" INTEGER NOT NULL,
+    "height" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "creatorId" TEXT NOT NULL,
+    "mapId" TEXT NOT NULL,
+
+    CONSTRAINT "Space_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SpaceElements" (
+    "id" TEXT NOT NULL,
+    "elementId" TEXT NOT NULL,
+    "spaceId" TEXT NOT NULL,
+    "x" INTEGER NOT NULL,
+    "y" INTEGER NOT NULL,
+
+    CONSTRAINT "SpaceElements_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Element" (
+    "id" TEXT NOT NULL,
+    "width" INTEGER NOT NULL,
+    "height" INTEGER NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+
+    CONSTRAINT "Element_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Map" (
+    "id" TEXT NOT NULL,
+    "width" INTEGER NOT NULL,
+    "height" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "thumbnailUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Map_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MapElements" (
+    "id" TEXT NOT NULL,
+    "mapId" TEXT NOT NULL,
+    "elementId" TEXT NOT NULL,
+    "x" INTEGER,
+    "y" INTEGER,
+
+    CONSTRAINT "MapElements_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Avatar" (
+    "id" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "name" TEXT,
+
+    CONSTRAINT "Avatar_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_providerId_key" ON "User"("providerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_tag_key" ON "User"("username", "tag");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_provider_providerId_key" ON "User"("provider", "providerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Space_id_key" ON "Space"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SpaceElements_id_key" ON "SpaceElements"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Element_id_key" ON "Element"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Map_id_key" ON "Map"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Map_name_key" ON "Map"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MapElements_id_key" ON "MapElements"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Avatar_id_key" ON "Avatar"("id");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_avatarId_fkey" FOREIGN KEY ("avatarId") REFERENCES "Avatar"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Space" ADD CONSTRAINT "Space_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Space" ADD CONSTRAINT "Space_mapId_fkey" FOREIGN KEY ("mapId") REFERENCES "Map"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SpaceElements" ADD CONSTRAINT "SpaceElements_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SpaceElements" ADD CONSTRAINT "SpaceElements_elementId_fkey" FOREIGN KEY ("elementId") REFERENCES "Element"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MapElements" ADD CONSTRAINT "MapElements_mapId_fkey" FOREIGN KEY ("mapId") REFERENCES "Map"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MapElements" ADD CONSTRAINT "MapElements_elementId_fkey" FOREIGN KEY ("elementId") REFERENCES "Element"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
