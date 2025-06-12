8b2ad672470e8e85d91794f7696928b3612786c9
prasanjit-dey-ux <prasanjitdey8523@gmail.com>
2025-06-12 17:58:15 +0000

    deleted old migrations

--- metaverse/packages/db/prisma/migrations/20250521111101_add_auth_provider/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "AuthProvider" AS ENUM ('EMAIL', 'GOOGLE', 'GITHUB');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "provider" "AuthProvider" NOT NULL DEFAULT 'EMAIL';
