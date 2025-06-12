44042753c101bbcf92008a8f32e93856e397d093
prasanjit-dey-ux <prasanjitdey8523@gmail.com>
2025-06-12 18:03:39 +0000

    fix some issues

--- metaverse/apps/http/src/index.ts
+++ metaverse/apps/http/src/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express from "express";
-import { router } from "./routes/v1";
+import { router as v1Router} from "./routes/v1";
 import cors from "cors";
 import client from "@metaverse/db/client"
 
@@ -14,7 +14,7 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use("/api/v1",router);
+app.use("/api/v1",v1Router);
 
 app.listen(PORT, () => {
     console.log(`Server is runninng on PORT ${PORT}`);  
--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -1,3 +1,4 @@
+// apps/http/src/routes/v1/index.ts
 import { Router } from "express";
 import { authRouter } from "./authRouter";
 import { userRouter } from "./userRouter";
@@ -5,18 +6,16 @@ import { adminRouter } from "./adminRouter";
 import { avatarRouter } from "./avatarRouter";
 import { spaceRouter } from "./spaceRouter";
 import { elementRouter } from "./elementRouter";
-import { requireAuth } from "../../middleware/authMiddleware";
+import { mapRouter } from "./mapRouter";
 
-export const router = Router();
+export const router = Router(); // This is the correct 'router' that 'index.ts' imports
 
 router.use("/auth", authRouter);
-
-
-router.use("/user", requireAuth, userRouter);
+router.use("/user", userRouter);
 router.use("/space", spaceRouter);
+router.use("/map", mapRouter);
+
 
 // router.use("/admin",adminRouter);
 // router.use("/avatar", avatarRouter);
-
 // router.use("/elements", elementRouter);
-
